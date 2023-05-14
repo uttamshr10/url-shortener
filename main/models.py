ad62d1e17568ef1f9352a42dfe75628b8eec7138
Uttam <utam.shrestha65@gmail.com>
2023-05-14 19:41:13 +0545

    ajax added and generate view is created

--- main/models.py
+++ main/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Link(models.Model):
     url = models.CharField(max_length=2000)
-    url_id = models.CharField(max_length=15)
+    uuid = models.CharField(max_length=15)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import uuid
+from main import models
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+def generate(request):
+    if request.method == 'POST':
+        url = request.POST['url']
+        ids = str(uuid.uuid4())[:4]
+        new_url = models.Link(url=url, uuid=ids)
+        new_url.save()
+        return HttpResponse(ids)
