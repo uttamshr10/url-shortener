d103b655fea3e7696de55251e35950b69ca5d22e
Uttam <utam.shrestha65@gmail.com>
2024-06-25 17:54:45 +0545

    Ready to Deploy

--- shorturl/settings.py
+++ shorturl/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,14 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!      
-SECRET_KEY = config('SECRET_KEY', default='')
+SECRET_KEY = 'ADFAKLJDFKLAJDKLFJALKSDJFLKJ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast='bool', default=True)
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    host.strip()
-    for host in config('ALLOWED_HOSTS', default=" ").split(',')
+    'localhost', '127.0.0.1', 'https://url-shortern-90ef3289316f.herokuapp.com'
 ]
 
 
@@ -117,3 +116,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEBUG = False
